b926fa8ecfa03ef605a084886a7dc9ade5259b01
Cary Carter <ccarterdev@gmail.com>
2024-08-29 11:31:57 -0700

    3 FILES ..:31:49.txt README.md main.py

--- main.py
+++ main.py
@@ -1,89 +1,50 @@
 #!/usr/bin/env python3
-from sys import argv
 import os
 import os.path as op
-from logfunc import logf
-from datetime import datetime as dt
-from datetime import timezone as tz
-from datetime import UTC
-from inspect import iscoroutinefunction as iscoroutine, isfunction as isfunc
-
-
-"""
-def apply_logf_all(scope: dict, *args, **kwargs):
-    _BUILTINS = [ k,v for k,v in __builtins__.__dict__.items() if callable(v)]
-    new_scope = {}
-    for fname, f in scope.copy().items():
-   
-
-        if any([isfunc(f), iscoroutine(f)]) and f.__name__ != 'apply_logf_all':
-            new_scope[fname] = logf(**kwargs)(f)
-
-    return new_scope
-"""
+from datetime import datetime as dt, UTC
 
 def newname():
     return dt.now(UTC).isoformat().split('.')[0] + '.txt'
 
 def getinput():
     inp = []
-    while True:
-        try:
+    try:
+        while True:
             print()
             inp.append(input('Enter: '))
-        except KeyboardInterrupt as e:
-            return '\n'.join(inp)
-        except:
-            raise
-        
+    except KeyboardInterrupt:
+        return '\n'.join(inp)
+
 def getcontent(fname: str):
     with open(fname, 'r') as f:
         return f.read()
-            
-def main():
 
-    with open(newname(), 'w') as f:
-        newinp = getinput()
-        if newinp.strip().replace('\r\n','').replace('\n',''):
-            f.write(newinp + '\n')
-            build_readme()
-        else:
-            build_readme()
-        print('\n')
-
-def cleanfiles(dpath: str = op.abspath('.')):
-    removed = []
+def cleanfiles(dpath: str = '.'):
     for root, _, files in os.walk(dpath):
-            for file in files:
-                if file.endswith(".txt"):
-                    if not getcontent(file).replace('\r\n','').replace('\n',''):
-                        removed.append(file)
-                        os.remove(file)
-    print('removed: ', removed)
-                    
+        for file in files:
+            filepath = op.join(root, file)
+            if file.endswith(".txt") and not getcontent(filepath).strip():
+                os.remove(filepath)
 
-def build_readme(repo_path: str = '.') -> None:
-    readme_path = op.join(op.abspath(repo_path), "README.md")
-
+def build_readme(repo_path: str = '.'):
     cleanfiles()
-    
+    readme_path = op.join(repo_path, "README.md")
     with open(readme_path, "w") as readme:
         readme.write("# Text Files\n\n")
-        
         for root, _, files in os.walk(repo_path):
             for file in files:
                 if file.endswith(".txt"):
-                    rel_path = os.path.relpath(os.path.join(root, file), repo_path)
-                    readme.write(f"- [{file}](/{rel_path}) " + getcontent(op.abspath(rel_path))[0:10] + '...\n')
+                    rel_path = os.path.relpath(op.join(root, file), repo_path)
+                    preview = getcontent(op.join(root, file))[:10] + '...'
+                    readme.write(f"- [{file}]({rel_path}) {preview}\n")
 
-
-_BUILTINS = [
-    name for name in dir(__builtins__) if callable(__builtins__.__dict__.get(name))
-]
+def main():
+    newinp = getinput()
+    if newinp.strip():
+        with open(newname(), 'w') as f:
+            f.write(newinp + '\n')
+    build_readme()
+    print('\n')
 
 if __name__ == '__main__':
-
-    #for k, v in apply_logf_all(locals().copy()).items():
-    #    if k not in _BUILTINS:
-    #        locals()[k] = v
     main()
