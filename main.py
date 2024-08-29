0f941428bee22b884e77cf18f29696af57e75b11
Cary Carter <ccarterdev@gmail.com>
2024-08-29 10:14:23 -0700

    2 FILES 2024-08-29T17:13:03.txt main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from sys import argv
+import os
+import os.path as op
+from logfunc import logf
+from datetime import datetime as dt
+from datetime import timezone as tz
+from datetime import UTC
+
+#os.environ['LOGF_USE_PRINT'] = 'True'
+
+
+@logf()
+def newname():
+    return dt.now(UTC).isoformat().split('.')[0] + '.txt'
+
+@logf()
+def getinput():
+    inp = []
+    while True:
+        try:
+            print()
+            inp.append(input('Enter: '))
+        except KeyboardInterrupt as e:
+            return '\n'.join(inp)
+        except:
+            raise
+        
+            
+def main():
+    with open(newname(), 'w') as f:
+        f.write(getinput() + '\n')
+        print('\n')
+
+if __name__ == '__main__':
+    main()
